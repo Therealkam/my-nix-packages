8091e0035e48fe1b08564719aac13a6f2ca3e4fa
Therealkam <andrea.camurri@realkam.net>
2021-10-03 19:09:45 +0200

    ADD - node Packages

---
+++ node-packages/generate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo ${DIR}
+node2nix=$(nix-build ../ --no-out-link -A nodePackages.node2nix)
+echo ${node2nix}
+
+cd ${DIR}
+rm -f ./node-env.nix
+${node2nix}/bin/node2nix -i node-packages.json -o node-packages.nix -c composition.nix
